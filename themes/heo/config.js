3c6cf1442fbec0baeded3103cf0abab0b255631c
RHZHZ <136958965+RHZHZ@users.noreply.github.com>
2026-02-07 19:28:10 +0800

    NotionNext项目地址

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -9,7 +9,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://rhzhz.cn' },
-    { title: '感谢tangly1024大佬的NotionNext项目', url: 'https://tangly1024.com' }
+    { title: 'NotionNext项目', url: 'https://github.com/tangly1024/NotionNext' }
   ],
 
   // 英雄区左右侧组件颠倒位置
