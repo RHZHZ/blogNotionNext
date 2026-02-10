6464a0fcddf04757044e428020d3a1a053ed6c24
RHZHZ <136958965+RHZHZ@users.noreply.github.com>
2026-02-10 21:30:15 +0800

    Update plugin.config.js

--- conf/plugin.config.js
+++ conf/plugin.config.js
@@ -3,6 +3,7 @@
  */
 module.exports = {
   // 网站全文搜索
+  SEARCH_ENGINE: process.env.NEXT_PUBLIC_SEARCH_ENGINE || 'local', // 搜索引擎：local (本地搜索), algolia (Algolia搜索)
   ALGOLIA_APP_ID: process.env.NEXT_PUBLIC_ALGOLIA_APP_ID || null, // 在这里查看 https://dashboard.algolia.com/account/api-keys/
   ALGOLIA_ADMIN_APP_KEY: process.env.ALGOLIA_ADMIN_APP_KEY || null, // 管理后台的KEY，不要暴露在代码中，在这里查看 https://dashboard.algolia.com/account/api-keys/
   ALGOLIA_SEARCH_ONLY_APP_KEY:
